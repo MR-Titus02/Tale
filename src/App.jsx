6ff8f1e04c0d98236c79833cc4f307148d7b3e88
MR-Titus02 <titusroxsan@gmail.com>
2025-04-22 15:07:26 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Footer from "./assets/Components/Footer";
+import Header from "./assets/Components/Header";
+import Section from "./assets/Components/Section";
+function App() {
+  return (
+    <>
+      <Header />
+      <Section />
+      <Footer />
+    </>
+  )
+}
+
+export default App
